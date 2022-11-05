8984915cee7439ea9456b68e460f7822a617c848
woo-ju-yeon <69452495+woo-ju-yeon@users.noreply.github.com>
2022-11-05 17:17:05 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+$(document).ready(function () {
+
+    alert("dfdfd");
+    renderCurrentTime();
+    renderQuote();
+});
+
+
+function renderCurrentTime() {
+
+
+    alert("dkfkf");
+let url = `https://worldtimeapi.org/api/timezone/Asia/Seoul`;
+fetch(url)
+.then(res => res.json()).then((data) => {
+    let datetime = data['datetime'].substr(11,5);
+    $('#time').text(datetime);
+});
+
+
+
+
+}
+
+
+function renderQuote() {
+    let url = `https://api.quotable.io/random`;
+    fetch(url)
+        .then(res => res.json()).then((data) => {
+            let content = `" ${data['content']} "`;
+            let author = `- ${data['author']} -`;
+            $('#content').text(content);
+            $('#author').text(author);
+        });
+}
